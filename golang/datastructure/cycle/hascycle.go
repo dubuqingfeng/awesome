146ad94d791c949717bcba583c96d5cf254162cd
Zhengyan Gao <gaozy@btcm.group>
2022-05-14 10:44:02 +0000

    add two pointer, in array.

---
+++ golang/datastructure/cycle/hascycle.go
@@ -0,0 +1,59 @@
+package cycle
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// two pointer
+func hasCycle(head *ListNode) bool {
+	var fast, slow *ListNode
+	fast = head
+	slow = head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	var head *ListNode
+	head = &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 3,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val: 5,
+						Next: &ListNode{
+							Val: 6,
+							Next: &ListNode{
+								Val: 7,
+								Next: &ListNode{
+									Val: 8,
+									Next: &ListNode{
+										Val: 9,
+										Next: &ListNode{
+											Val:  1,
+											Next: nil,
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	fmt.Println(hasCycle(head))
+}
